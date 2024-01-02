b3967e6fafa01cab377f4cf405b92399ae64a455
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-02 22:41:18 +0200

    extended JpaRepository and added updateBook

--- src/main/java/mate/academy/springboot/repository/BookRepository.java
+++ src/main/java/mate/academy/springboot/repository/BookRepository.java
@@ -1,13 +1,23 @@
 package mate.academy.springboot.repository;
 
-import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
 import mate.academy.springboot.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    Optional<Book> findBookById(Long id);
-
-    List<Book> findAll();
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("UPDATE Book b SET b.title = :title,"
+            + " b.author = :author,"
+            + " b.isbn = :isbn,"
+            + " b.price = :price,"
+            + " b.description = :description,"
+            + " b.coverImage = :coverImage WHERE b.id = :id")
+    void updateBook(Long id,
+                    String title,
+                    String author,
+                    String isbn,
+                    BigDecimal price,
+                    String description,
+                    String coverImage);
 }
+
