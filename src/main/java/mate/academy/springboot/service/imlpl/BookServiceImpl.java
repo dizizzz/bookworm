b20a026627456604b91d58d3d8fe26530a3d3437
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-02 22:41:40 +0200

    added methods updateBookById and deleteById

--- src/main/java/mate/academy/springboot/service/imlpl/BookServiceImpl.java
+++ src/main/java/mate/academy/springboot/service/imlpl/BookServiceImpl.java
@@ -34,8 +34,28 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto getBookById(Long id) {
-        Book book = bookRepository.findBookById(id).orElseThrow(
+        Book book = bookRepository.findById(id).orElseThrow(
                     () -> new EntityNotFoundException("Can`t find book by id " + id));
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto updateBookById(Long id, CreateBookRequestDto requestDto) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id " + id));
+
+        book.setTitle(requestDto.getTitle());
+        book.setAuthor(requestDto.getAuthor());
+        book.setIsbn(requestDto.getIsbn());
+        book.setPrice(requestDto.getPrice());
+        book.setDescription(requestDto.getDescription());
+        book.setCoverImage(requestDto.getCoverImage());
+
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
     }
 }
