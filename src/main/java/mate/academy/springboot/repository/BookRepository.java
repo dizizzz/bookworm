aeba2c53db4e323c9cfb8ef59a0a80ebc68b4e7d
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-03 16:23:25 +0200

    delete unnecessary method

--- src/main/java/mate/academy/springboot/repository/BookRepository.java
+++ src/main/java/mate/academy/springboot/repository/BookRepository.java
@@ -1,23 +1,8 @@
 package mate.academy.springboot.repository;
 
-import java.math.BigDecimal;
 import mate.academy.springboot.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query("UPDATE Book b SET b.title = :title,"
-            + " b.author = :author,"
-            + " b.isbn = :isbn,"
-            + " b.price = :price,"
-            + " b.description = :description,"
-            + " b.coverImage = :coverImage WHERE b.id = :id")
-    void updateBook(Long id,
-                    String title,
-                    String author,
-                    String isbn,
-                    BigDecimal price,
-                    String description,
-                    String coverImage);
 }
 
