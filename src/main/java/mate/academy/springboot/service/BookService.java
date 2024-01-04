c2029f51ca93ea542b84f2a008be0a1cb8806324
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-04 20:17:01 +0200

    added method search()

--- src/main/java/mate/academy/springboot/service/BookService.java
+++ src/main/java/mate/academy/springboot/service/BookService.java
@@ -2,6 +2,7 @@ package mate.academy.springboot.service;
 
 import java.util.List;
 import mate.academy.springboot.dto.BookDto;
+import mate.academy.springboot.dto.BookSearchParameters;
 import mate.academy.springboot.dto.CreateBookRequestDto;
 
 public interface BookService {
@@ -14,4 +15,6 @@ public interface BookService {
     BookDto updateBookById(Long id, CreateBookRequestDto requestDto);
 
     void deleteById(Long id);
+
+    List<BookDto> search(BookSearchParameters params);
 }
