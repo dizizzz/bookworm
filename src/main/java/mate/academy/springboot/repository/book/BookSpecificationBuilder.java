35b51d6868872ff8d6087e562880ecac7a969700
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-04 20:16:22 +0200

    added BookSpecificationBuilder

---
+++ src/main/java/mate/academy/springboot/repository/book/BookSpecificationBuilder.java
@@ -0,0 +1,31 @@
+package mate.academy.springboot.repository.book;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.dto.BookSearchParameters;
+import mate.academy.springboot.model.Book;
+import mate.academy.springboot.repository.SpecificationBuilder;
+import mate.academy.springboot.repository.SpecificationProviderManager;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
+
+    @Override
+    public Specification<Book> build(BookSearchParameters searchParameters) {
+        Specification<Book> specification = Specification.where(null);
+        if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
+            specification = specification.and(bookSpecificationProviderManager
+                    .getSpecificationProvider("title")
+                    .getSpecification(searchParameters.titles()));
+        }
+        if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
+            specification = specification.and(bookSpecificationProviderManager
+                    .getSpecificationProvider("author")
+                    .getSpecification(searchParameters.authors()));
+        }
+        return specification;
+    }
+}
