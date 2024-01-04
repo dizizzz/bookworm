84fb9f62e089d94f050dc7d7eade2c2028d18c08
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-04 20:14:51 +0200

    added SpecificationBuilder

---
+++ src/main/java/mate/academy/springboot/repository/SpecificationBuilder.java
@@ -0,0 +1,8 @@
+package mate.academy.springboot.repository;
+
+import mate.academy.springboot.dto.BookSearchParameters;
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SpecificationBuilder<T> {
+    Specification<T> build(BookSearchParameters searchParameters);
+}
