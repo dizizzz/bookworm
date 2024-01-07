04d0cd579614b3eb0894f0c13c18563bec87de62
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-07 18:50:11 +0200

    added annotations

--- src/main/java/mate/academy/springboot/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springboot/dto/CreateBookRequestDto.java
@@ -1,9 +1,11 @@
 package mate.academy.springboot.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import mate.academy.springboot.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
@@ -11,10 +13,12 @@ public class CreateBookRequestDto {
     private String title;
     @NotNull
     private String author;
+    @Isbn
     private String isbn;
     @NotNull
     @Min(0)
     private BigDecimal price;
+    @NotBlank
     private String description;
     private String coverImage;
 }
