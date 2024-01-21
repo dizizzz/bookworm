3fde980c1ee48457e7d5a28f614f7069ff4b1c34
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-22 01:36:50 +0200

    added UserDto

---
+++ src/main/java/mate/academy/springboot/dto/UserRegistrationRequestDto.java
@@ -0,0 +1,37 @@
+package mate.academy.springboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import mate.academy.springboot.validation.FieldMatch;
+import org.hibernate.validator.constraints.Length;
+
+@FieldMatch.List({
+        @FieldMatch(
+                field = "password",
+                fieldMatch = "repeatPassword",
+                message = "Passwords do not match!"
+        )
+})
+@Data
+public class UserRegistrationRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Length(min = 8, max = 20)
+    private String password;
+
+    @NotBlank
+    @Length(min = 8, max = 20)
+    private String repeatPassword;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private String shippingAddress;
+}
