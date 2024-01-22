8f0e56abea44ef6da86083ee245ac80910ce68cf
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-22 22:17:17 +0200

    added the method toModel()

--- src/main/java/mate/academy/springboot/mapper/UserMapper.java
+++ src/main/java/mate/academy/springboot/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package mate.academy.springboot.mapper;
 
 import mate.academy.springboot.config.MapperConfig;
+import mate.academy.springboot.dto.UserRegistrationRequestDto;
 import mate.academy.springboot.dto.UserResponseDto;
 import mate.academy.springboot.model.User;
 import org.mapstruct.Mapper;
@@ -8,4 +9,6 @@ import org.mapstruct.Mapper;
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
     UserResponseDto toDto(User user);
+
+    User toModel(UserRegistrationRequestDto requestDto);
 }
