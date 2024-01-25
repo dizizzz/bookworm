dd997a8114bdf6a09d2cc4e495e3b70713df3ade
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-26 00:51:29 +0200

    added entity Role

---
+++ src/main/java/mate/academy/springboot/model/Role.java
@@ -0,0 +1,25 @@
+package mate.academy.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar",
+            name = "name", unique = true, nullable = false)
+    private RoleName name;
+}
