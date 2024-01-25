eae5cb9a605349db2baef9d09dc1c3ca86f8b67e
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-26 00:55:42 +0200

    used PasswordEncoder

--- src/main/java/mate/academy/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/springboot/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package mate.academy.springboot.service.imlpl;
+package mate.academy.springboot.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import mate.academy.springboot.dto.UserRegistrationRequestDto;
@@ -8,6 +8,7 @@ import mate.academy.springboot.mapper.UserMapper;
 import mate.academy.springboot.model.User;
 import mate.academy.springboot.repository.user.UserRepository;
 import mate.academy.springboot.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -23,7 +25,13 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsByEmail(email)) {
             throw new RegistrationException("Can`t register user by email: " + email);
         }
-        User user = userMapper.toModel(requestDto);
+        User user = new User();
+        user.setEmail(requestDto.getEmail());
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.setLastName(requestDto.getLastName());
+        user.setFirstName(requestDto.getFirstName());
+        user.setShippingAddress(requestDto.getShippingAddress());
+
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
