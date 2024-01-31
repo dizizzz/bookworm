433b9718692639101dbb829fee41a2f24eab603c
Solomia Bobyak <mia.bobyak@gmail.com>
2024-01-31 23:11:13 +0200

    added methods

--- src/main/java/mate/academy/springboot/repository/book/BookRepository.java
+++ src/main/java/mate/academy/springboot/repository/book/BookRepository.java
@@ -1,11 +1,15 @@
 package mate.academy.springboot.repository.book;
 
+import java.util.List;
 import mate.academy.springboot.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Query("FROM Book b INNER JOIN FETCH b.categories c WHERE c.id = :category_id")
+    List<Book> findAllByCategoriesId(@Param("category_id") Long categoryId);
 }
-
--- src/main/java/mate/academy/springboot/service/BookService.java
+++ src/main/java/mate/academy/springboot/service/BookService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import mate.academy.springboot.dto.book.BookDto;
 import mate.academy.springboot.dto.book.BookSearchParameters;
 import mate.academy.springboot.dto.book.CreateBookRequestDto;
+import mate.academy.springboot.model.Book;
 import org.springframework.data.domain.Pageable;
 
 public interface BookService {
@@ -18,4 +19,6 @@ public interface BookService {
     void deleteById(Long id);
 
     List<BookDto> search(BookSearchParameters params);
+
+    List<Book> findAllByCategoriesId(Long id);
 }
