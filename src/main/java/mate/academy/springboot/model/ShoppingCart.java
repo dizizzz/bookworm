6511484a1d2847d6441217a6181d867db7325698
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-03 19:11:19 +0200

    added entity ShoppingCart

---
+++ src/main/java/mate/academy/springboot/model/ShoppingCart.java
@@ -0,0 +1,37 @@
+package mate.academy.springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shopping_cart")
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(
+            mappedBy = "shoppingCart",
+            fetch = FetchType.EAGER)
+    private Set<CartItem> cartItems = new HashSet<>();
+}
