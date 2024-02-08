82e36b24965f854966b32a86b746fb53b0b75b6c
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-08 02:30:03 +0200

    add annotation

--- src/main/java/mate/academy/springboot/repository/order/OrderItemRepository.java
+++ src/main/java/mate/academy/springboot/repository/order/OrderItemRepository.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import mate.academy.springboot.model.Order;
 import mate.academy.springboot.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findAllByOrder(Order order);
 
-    Optional<OrderItem> findByIdAndOrderId(Long itemId, Long orderId);
+    Optional<OrderItem> findByIdAndOrderId(@Param("itemId") Long itemId,
+                                            @Param("orderId") Long orderId);
 }
--- src/main/java/mate/academy/springboot/repository/order/OrderRepository.java
+++ src/main/java/mate/academy/springboot/repository/order/OrderRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 import mate.academy.springboot.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findByIdAndUserId(Long userId, Long orderId);
+    Optional<Order> findByIdAndUserId(@Param("userId") Long userId,
+                                     @Param("orderId") Long orderId);
 
-    List<Order> findAllByUserId(Long userId);
+    List<Order> findAllByUserId(@Param("userId") Long userId);
 }
