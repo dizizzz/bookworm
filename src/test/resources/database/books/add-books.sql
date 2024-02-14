a5a167b4d3ee2691680a302d8a23cc2d811bee5c
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-14 22:25:56 +0200

    added sql scripts

---
+++ src/test/resources/database/books/add-books.sql
@@ -0,0 +1,5 @@
+insert into books (id, title, author, isbn, price, description, coverImage)
+values (1, "Title 1", "Author 1", "1248752418855", 123, "Description 1", "image1.jpg");
+
+insert into books (id, title, author, isbn, price, description, coverImage)
+values (2, "Title 2", "Author 2", "1248752418856", 124, "Description 2", "image2.jpg");
---
+++ src/test/resources/database/categories/add-categories.sql
@@ -0,0 +1,2 @@
+INSERT INTO categories (id, name, description) values (1, "Category 1", "Description 1");
+INSERT INTO categories (id, name, description) values (2, "Category 2", "Description 2");
