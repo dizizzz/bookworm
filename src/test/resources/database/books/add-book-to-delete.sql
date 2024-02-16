1c5b9d8b6f5db992dd23c8987ba9e83ba55ef742
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-16 23:54:20 +0200

    added sql scripts

---
+++ src/test/resources/database/books/add-book-to-delete.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, categories)
+VALUES (3, "Title 3", "Author 3", "1207199818863", 333, "Description 3", "image3.jpg", 3);
--- src/test/resources/database/books/add-books.sql
+++ src/test/resources/database/books/add-books.sql
@@ -1,5 +1,5 @@
-insert into books (id, title, author, isbn, price, description, coverImage)
-values (1, "Title 1", "Author 1", "1248752418855", 123, "Description 1", "image1.jpg");
+INSERT INTO books (id, title, author, isbn, price, description, cover_image)
+VALUES (1, "Title 1", "Author 1", "1207199818861", 111, "Description 1", "image1.jpg");
 
-insert into books (id, title, author, isbn, price, description, coverImage)
-values (2, "Title 2", "Author 2", "1248752418856", 124, "Description 2", "image2.jpg");
+INSERT INTO books (id, title, author, isbn, price, description, cover_image)
+VALUES (2, "Title 2", "Author 2", "1207199818862", 222, "Description 2", "image2.jpg");
