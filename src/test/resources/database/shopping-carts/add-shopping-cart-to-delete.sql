85ea543d6505dae3595e7971df3c8a45e69c9ca5
Solomia Bobyak <mia.bobyak@gmail.com>
2024-02-20 17:41:23 +0200

    added scripts

--- src/test/resources/database/shopping-carts/add-shopping-cart-to-delete.sql
+++ src/test/resources/database/shopping-carts/add-shopping-cart-to-delete.sql
@@ -1,1 +1,1 @@
-INSERT INTO shopping_carts (id, user_id) VALUES (3, 1);
+INSERT INTO shopping_carts (id, user_id) VALUES (3, 3);
--- src/test/resources/database/users/add-users.sql
+++ src/test/resources/database/users/add-users.sql
@@ -2,3 +2,5 @@ INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
 VALUES (1, "sam@email.com", "123456789", "Sam", "Smith", "123 Main St, City, Country");
 INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
 VALUES (2, "susanna@email.com", "123456789", "Susanna", "Smith", "123 Main St, City, Country");
+INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
+VALUES (3, "simon@email.com", "123456789", "Simon", "Smith", "123 Main St, City, Country");
